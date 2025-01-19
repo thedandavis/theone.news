d8a61e16a328227ef101c2520983a6856e6dd1a5
Dan Davis <dan@thedandavis.com>
2025-01-19 05:35:01 -0800

    Mother Earth

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-/*   { name: "Projects", href: "/projects" },
- */  { name: "Contact", href: "/contact" },
+  { name: "Stories", href: "/stories" },
+  { name: "Contact", href: "/contact" },
 ];
 
 export default function Home() {
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Inspired by The Universe.
+          Inspired by The Universe. Cared for by Mother Earth.
         </h2>
       </div>
     </div>
