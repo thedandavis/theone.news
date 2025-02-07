cdb6269b8e5d369cd6ad14cdf102cb9ec74ded03
Dan Davis <dan@thedandavis.com>
2025-02-06 20:15:42 -0800

    Installed SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: {
@@ -69,6 +70,7 @@ export default function RootLayout({
         className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
           }`}
       >
+        <SpeedInsights />
         {children}
       </body>
     </html>
