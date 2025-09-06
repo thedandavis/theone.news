0922fcb6ebab6f2a7cf67324cb7d4438fc59a459
Daniel (Dan) Roy Davis <dan@thedandavis.com>
2025-09-06 15:14:30 -0700

    New Solgan

--- app/page.tsx
+++ app/page.tsx
@@ -32,13 +32,13 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Please Help
+        The One
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Gangstalking attack victim, isolated, in need of support...paralizing effects from devices harassing me non-stop in my house.
+          Inspired By Space. Inspired by You. Inspiring Each Other.
         </h2>
       </div>
     </div>
