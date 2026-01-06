0d39f109bee9dbd9d1489a453d2be8067ec1c13e
Daniel (Dan) Roy Davis <dan@thedandavis.com>
2026-01-06 10:12:41 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-Medical / Tech attack victim, needs support and help contacting proper authorities.
+Inspired By The Universe. Inspired by You. Inspiring Each Other.
       </div
     </div>
   );
