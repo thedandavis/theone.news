1a6c54320a090ddb80f565bdcd12a778548eb32a
Daniel (Dan) Roy Davis <dan@thedandavis.com>
2026-04-22 22:18:39 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             >
               {item.name}
             </Link>
-          )}
+          ))}
         </ul>
       </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
